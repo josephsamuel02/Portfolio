8eac52730b3b123b55f0e87a366c1be7c3027d24
josephsamuel02 <mrsamd02@gmail.com>
2022-10-25 18:29:20 +0100

    .

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "./modules/public/components/nav";
-// import About from "./modules/public/pages/about";
-// import Contact from "./modules/public/pages/contact";
 import LandingPage from "./modules/public/pages/landingPage";
 import ROUTES from "./modules/public/public.routes";
 const Router = () => {
@@ -10,11 +8,7 @@ const Router = () => {
 			<Nav />
 
 			<Routes>
-				{/* {PublicRouter()} */}
-
 				<Route path={ROUTES.HOME} element={<LandingPage />} />
-				{/* <Route path={ROUTES.ABOUT} element={<About />} /> */}
-				{/* <Route path={ROUTES.CONTACT} element={<Contact />} /> */}
 				<Route path={"*"} element={<h1 className=" mx-auto my-40 items-center text-center text-red-600">NO Page Here</h1>} />
 			</Routes>
 		</>
